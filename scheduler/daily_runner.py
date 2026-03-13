96492a05115e4c2995e090b7118a0ae540679363
shivamgravity <shivamgravity@gmail.com>
2026-03-13 06:35:21 +0000

    added daily runner file

---
+++ scheduler/daily_runner.py
@@ -0,0 +1,17 @@
+import schedule
+import time
+from agents.student_agent import student_agent
+
+def run_daily_check():
+
+    result = student_agent.run(
+        "Check assignments and generate study plan"
+    )
+
+    print(result)
+
+schedule.every().day.at("08:00").do(run_daily_check)
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
