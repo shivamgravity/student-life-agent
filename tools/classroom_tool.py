38f45d50d6fac5f3b94224b019d820dc010d1489
shivamgravity <shivamgravity@gmail.com>
2026-03-13 06:23:09 +0000

    added tool - classroom in the codebase

---
+++ tools/classroom_tool.py
@@ -0,0 +1,8 @@
+def get_assignments():
+
+    assignments = [
+        {"title": "ML Report", "due": "2026-03-17"},
+        {"title": "NLP Lab", "due": "2026-03-18"}
+    ]
+
+    return assignments
