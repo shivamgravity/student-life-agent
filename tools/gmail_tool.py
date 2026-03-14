277751dd30bdac13730fa48256934fe9c0a32f6e
Shivam Gravity <shivamgravity@gmail.com>
2026-03-14 18:02:56 +0530

    tools/gmail_tools.py - added the email subject and content

---
+++ tools/gmail_tool.py
@@ -0,0 +1,10 @@
+def scan_emails():
+
+    emails = [
+        {
+            "subject": "New Assignment",
+            "content": "Submit NLP Lab before March 18"
+        }
+    ]
+
+    return emails
