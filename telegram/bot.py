0d01be4ed9256d07754504ed601f4429f3a52282
Shivam Gravity <shivamgravity@gmail.com>
2026-03-16 16:31:03 +0530

    telegram/bot.py - added telegram token from env

--- telegram/bot.py
+++ telegram/bot.py
@@ -1,6 +1,12 @@
 from telegram.ext import ApplicationBuilder, MessageHandler, filters
 from telegram import Update
 import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 API_URL = "http://127.0.0.1:8000/ask"
 
