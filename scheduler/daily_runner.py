3ceb04db9de557d7d2799f85491b5de64faf76ed
Shivam Gravity <shivamgravity@gmail.com>
2026-03-17 17:28:46 +0530

    updated daily runner python file

--- scheduler/daily_runner.py
+++ scheduler/daily_runner.py
@@ -1,5 +1,12 @@
 import schedule # type: ignore
 import time
+import os
+import sys
+
+target_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'agents'))
+sys.path.append(target_dir)
+target_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'telegram'))
+sys.path.append(target_dir)
 
 from agents.student_agent import student_agent
 from telegram.send_message import send_telegram_message
@@ -23,8 +30,8 @@ def run_daily_check():
 
 
 # Run every day at 8 AM
-schedule.every().day.at("08:00").do(run_daily_check)
-
+# schedule.every().day.at("08:00").do(run_daily_check)
+schedule.every(1).minutes.do(run_daily_check)
 
 while True:
     schedule.run_pending()
