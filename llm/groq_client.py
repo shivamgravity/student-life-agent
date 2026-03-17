ed0406115fb5c525663a9334348256337b92c9a3
Shivam Gravity <shivamgravity@gmail.com>
2026-03-17 15:17:49 +0530

    added the groq_client file in the codebase

---
+++ llm/groq_client.py
@@ -0,0 +1,20 @@
+import os
+from groq import Groq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+def ask_llm(prompt):
+
+    response = client.chat.completions.create(
+        model="llama3-8b-8192",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    return response.choices[0].message.content
