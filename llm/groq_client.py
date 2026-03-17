c35e1b03649e228ed9e835078e97c2eb3a36f1cb
Shivam Gravity <shivamgravity@gmail.com>
2026-03-17 15:24:16 +0530

    updated the groq api llama-3.1-8b-instant model

--- llm/groq_client.py
+++ llm/groq_client.py
@@ -11,7 +11,7 @@ client = Groq(
 def ask_llm(prompt):
 
     response = client.chat.completions.create(
-        model="llama3-8b-8192",
+        model="llama-3.1-8b-instant",
         messages=[
             {"role": "user", "content": prompt}
         ]
