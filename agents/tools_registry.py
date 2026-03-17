3e44d1d9b68f62ea6919e144cf67968db6c0a4a9
Shivam Gravity <shivamgravity@gmail.com>
2026-03-17 16:01:15 +0530

    added risk detector tool in registry file

--- agents/tools_registry.py
+++ agents/tools_registry.py
@@ -1,5 +1,7 @@
 from tools.classroom_tool import get_assignments
+from tools.risk_detector_tool import detect_risks
 
 TOOLS = {
-    "get_assignments": get_assignments
+    "get_assignments": get_assignments,
+    "detect_risks": detect_risks
 }
